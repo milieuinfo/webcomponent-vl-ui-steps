583c393bd816d40d66f91c7885b2ef12de63d959
Tom Coemans <tom.coemans@gmail.com>
2020-04-22 09:04:58 +0200

    dist

--- dist/vl-steps-all.src.js
+++ dist/vl-steps-all.src.js
@@ -1,6 +1,6 @@
-import { VlSteps } from 'vl-ui-steps/dist/vl-steps.js';
-import { VlStep } from 'vl-ui-steps/dist/vl-step.js';
-import { VlDurationStep } from 'vl-ui-steps/dist/vl-duration-step.js';
+import { VlSteps } from 'vl-ui-steps/dist/vl-steps.src.js';
+import { VlStep } from 'vl-ui-steps/dist/vl-step.src.js';
+import { VlDurationStep } from 'vl-ui-steps/dist/vl-duration-step.src.js';
 
 export {
     VlSteps, 
