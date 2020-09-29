5bc3011be0e015179cd94db76682e5663292d5f8
Tom Coemans <tom.coemans@gmail.com>
2020-09-29 10:29:40 +0200

    dist build

--- dist/vl-step.src.js
+++ dist/vl-step.src.js
@@ -12,6 +12,7 @@ import '@govflanders/vl-ui-accordion/dist/js/accordion.js';
  *
  * @property {boolean} data-vl-disabled - Attribuut om aan te geven dat de stap niet toegankelijk is.
  * @property {(success | warning | error)} data-vl-type - Attribuut bepaalt het type van de stap.
+ * @property {boolean} data-vl-toggleable - Attribuut wordt gebruikt om ervoor te zorgen dat de textarea getoond wordt als een block element en bijgevolg de breedte van de parent zal aannemen.
  *
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-steps/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-steps/issues|Issues}
@@ -20,7 +21,7 @@ import '@govflanders/vl-ui-accordion/dist/js/accordion.js';
  */
 export class VlStep extends vlElement(HTMLElement) {
   static get _observedAttributes() {
-    return ['type', 'accordion'];
+    return ['type', 'toggleable'];
   }
 
   static get _observedChildClassAttributes() {
@@ -37,6 +38,7 @@ export class VlStep extends vlElement(HTMLElement) {
           <div class="vl-step__header">
             <div class="vl-step__header__titles">
               <h3 id="title" class="vl-step__title">
+                <span id="title-label"></span>
                 <span id="title-annotation" class="vl-step__title__annotation"></span>
               </h3>
               <p id="sub-title" class="vl-step__subtitle"></p>
@@ -51,9 +53,13 @@ export class VlStep extends vlElement(HTMLElement) {
     this._processSlots();
   }
 
+  /**
+   * Geeft de step template.
+   * @return {HTMLElement}
+   */
   get template() {
     const template = this._element.cloneNode(true);
-    if (this._isAccordion) {
+    if (this._isToggleable) {
       vl.accordion.dress(template);
       template.querySelector('#content').addEventListener('click', (e) => e.stopPropagation());
     }
@@ -100,11 +106,11 @@ export class VlStep extends vlElement(HTMLElement) {
     return 'vl-step--';
   }
 
-  get _isAccordion() {
-    return this.hasAttribute('accordion');
+  get _isToggleable() {
+    return this.hasAttribute('toggleable');
   }
 
-  _getAccordionHeaderHTML() {
+  _getToggleableHeaderHTML() {
     return `
       <button class="vl-step__header js-vl-accordion__toggle">
         <div class="vl-step__header__titles">
@@ -123,12 +129,12 @@ export class VlStep extends vlElement(HTMLElement) {
     this._changeClass(this._element, oldValue, newValue, this._classPrefix);
   }
 
-  _accordionChangedCallback(oldValue, newValue) {
+  _toggleableChangedCallback(oldValue, newValue) {
     if (newValue != undefined) {
       this._element.classList.add('vl-step--accordion');
       this._element.classList.add('js-vl-accordion');
       this._headerElement.remove();
-      this._wrapperElement.insertAdjacentHTML('afterbegin', this._getAccordionHeaderHTML());
+      this._wrapperElement.insertAdjacentHTML('afterbegin', this._getToggleableHeaderHTML());
       this.__processSlot(this.querySelector('[slot="title"]'), (slot) => this._titleElement.prepend(slot));
       this.__processSlot(this.querySelector('[slot="title-label"]'), (slot) => this._titleLabelElement.prepend(slot));
     }
@@ -138,6 +144,7 @@ export class VlStep extends vlElement(HTMLElement) {
     this.__processSlot(this.querySelector('[slot="identifier"]'), (slot) => this._iconElement.prepend(slot));
     this.__processSlot(this.querySelector('[slot="identifier-annotation"]'), (slot) => this._subIconElement.append(slot));
     this.__processSlot(this.querySelector('[slot="title"]'), (slot) => this._titleElement.prepend(slot));
+    this.__processSlot(this.querySelector('[slot="title-label"]'), (slot) => this._titleLabelElement.prepend(slot));
     this.__processSlot(this.querySelector('[slot="title-annotation"]'), (slot) => this._titleAnnotationElement.append(slot), () => this._titleAnnotationElement.hidden = true);
     this.__processSlot(this.querySelector('[slot="sub-title"]'), (slot) => this._subTitleElement.append(slot));
     this.__processSlot(this.querySelector('[slot="content"]'), (slot) => this._contentElement.append(slot), () => this._contentElement.hidden = true);
