40ea8216efa98f83d1208862db0bb33a1bdd8e67
Tom Coemans <tom.coemans@gmail.com>
2021-01-28 18:30:20 +0100

    ES export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export * from '../dist/vl-steps-all.src.js';
+import {VlSteps} from '../dist/vl-steps.src.js';
+import {VlStep} from '../dist/vl-step.src.js';
+import {VlDurationStep} from '../dist/vl-duration-step.src.js';
+
+export {
+  VlSteps,
+  VlStep,
+  VlDurationStep,
+};
