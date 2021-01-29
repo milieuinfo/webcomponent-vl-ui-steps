17cbe44c3b2489f743d3608074730fb0b6e6cf1a
Tom Coemans <tom.coemans@gmail.com>
2021-01-29 12:46:57 +0100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import {VlSteps} from '../dist/vl-steps.src.js';
-import {VlStep} from '../dist/vl-step.src.js';
-import {VlDurationStep} from '../dist/vl-duration-step.src.js';
-
-export {
-  VlSteps,
-  VlStep,
-  VlDurationStep,
-};
+export * from '../dist/vl-steps-all.src.js';
